0fcf0033ed3ff8cd3f8bcfd5556b3296666f6cf3
Rafael Arenhart <rafael.arenhart@ltrace.com.br>
2022-07-27 17:05:05 -0300

    tests passed

--- PyEDT/interface.py
+++ PyEDT/interface.py
@@ -35,9 +35,9 @@ VOXELS_PER_THREAD = math.ceil(VOL_EDGE/MAX_THREADS_PER_BLOCK)
 THREADS_PER_BLOCK = math.ceil(VOL_EDGE/VOXELS_PER_THREAD)
 A = np.zeros((VOL_EDGE, VOL_EDGE, VOL_EDGE), dtype = np.uint32)
 A[VOL_EDGE//4:3*VOL_EDGE//4, VOL_EDGE//4:3*VOL_EDGE//4, VOL_EDGE//4:3*VOL_EDGE//4] = 1
-C = np.empty_like(A)
 print(VOXELS_PER_THREAD, THREADS_PER_BLOCK)
 BASE_DEVICE_MEMORY_USE = 5000000
+
 
 def edt_gpu(A):
     gpu = cuda.get_current_device()
@@ -54,9 +54,58 @@ def edt_gpu(A):
         gedt = gedt_compiler(line_length, voxels_per_thread)
         gedt[(grid_dim_1, grid_dim_2), threads_per_block](A_d)
 
-    C = A_d.copy_to_host()
+    B = A_d.copy_to_host()
     del A_d
-    return C
+    return B
+
+
+def edt_gpu_split(A, min_splits=None):
+    gpu = cuda.get_current_device()
+    max_threads_per_block = gpu.MAX_THREADS_PER_BLOCK
+    x_dim, y_dim, z_dim = A.shape
+    segments = 2
     
-def edt_cpu():
-    pass
+    for grid_axis_1, grid_axis_2, line_axis, gedt_compiler in (
+    (1, 2, 0, compile_gedt_x),
+    (0, 2, 1, compile_gedt_y),
+    (0, 1, 2, compile_gedt_z)
+    ):
+        grid_dim_1 = A.shape[grid_axis_1]
+        grid_dim_2 = A.shape[grid_axis_2]
+        line_length = A.shape[line_axis]
+        
+        compiled_gedts = dict()
+        voxels_per_thread = math.ceil(line_length/max_threads_per_block)
+        threads_per_block = math.ceil(line_length/voxels_per_thread)
+    
+        slices_tuple = [(slice(None),
+                         slice((i)*grid_dim_1//(segments), (i+1)*grid_dim_1//(segments)),
+                         slice((j)*grid_dim_2//(segments), (j+1)*grid_dim_2//(segments)))
+                         for i in range(segments) for j in range(segments)]
+
+        for slices in slices_tuple:
+            if (line_length, voxels_per_thread) not in compiled_gedts.keys():
+                compiled_gedts[(line_length, voxels_per_thread)] = gedt_compiler(line_length, voxels_per_thread)
+            gedt = compiled_gedts[(line_length, voxels_per_thread)]
+            ordered_slices = dict()
+            ordered_slices[line_axis] = slices[0]
+            ordered_slices[grid_axis_1] = slices[1]
+            ordered_slices[grid_axis_2] = slices[2]
+            A_d = cuda.to_device(np.ascontiguousarray(A[ordered_slices[0], ordered_slices[1], ordered_slices[2]]))
+            gedt[(slices[1].stop - slices[1].start, slices[2].stop - slices[2].start), threads_per_block](A_d)    
+            A[ordered_slices[0], ordered_slices[1], ordered_slices[2]] = A_d.copy_to_host()
+
+    del A_d
+    print(A.sum())
+    A.tofile("test.raw")
+    return A
+    
+    
+def edt_cpu(A):
+    B = np.empty_like(A)
+    single_pass_erosion_x(A, B)
+    single_pass_erosion_y(B)
+    single_pass_erosion_z(B)
+    return B
+    
+    
--- tests/test_pyedt.py
+++ tests/test_pyedt.py
@@ -2,7 +2,7 @@ import pathlib
 
 import numpy as np
 
-from PyEDT.interface import edt_gpu, edt_cpu
+from PyEDT.interface import edt_gpu, edt_gpu_split, edt_cpu
 
 reference = np.fromfile(pathlib.Path(__file__).parent/"20_edt.raw", dtype=np.uint32).reshape(20, 20, 20)
 EDGE_SIZE = 20
@@ -15,7 +15,15 @@ def test_edt_gpu():
     assert(np.all(edt_gpu(array) == reference))
 
 def test_edt_gpu_split():
-    pass
+    array = np.zeros((EDGE_SIZE, EDGE_SIZE, EDGE_SIZE), dtype=np.uint32)
+    array[EDGE_SIZE//4:3*EDGE_SIZE//4,
+          EDGE_SIZE//4:3*EDGE_SIZE//4,
+          EDGE_SIZE//4:3*EDGE_SIZE//4] = 1
+    assert(np.all(edt_gpu_split(array) == reference))
     
-def test_est_cpu():
-    pass
+def test_edt_cpu():
+    array = np.zeros((EDGE_SIZE, EDGE_SIZE, EDGE_SIZE), dtype=np.uint32)
+    array[EDGE_SIZE//4:3*EDGE_SIZE//4,
+          EDGE_SIZE//4:3*EDGE_SIZE//4,
+          EDGE_SIZE//4:3*EDGE_SIZE//4] = 1
+    assert(np.all(edt_cpu(array) == reference))
