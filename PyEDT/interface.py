19f97812582272fa2326d41f81728f88e2a4086b
Rafael Arenhart <rafael.arenhart@ltrace.com.br>
2022-07-28 11:48:37 -0300

    auto edt method

--- PyEDT/interface.py
+++ PyEDT/interface.py
@@ -6,37 +6,10 @@ import numpy as np
 
 from .functions import *
 
-cuda.select_device(0)
-print(cuda.gpus[0].name)
-cuda.is_available()
-cuda.detect()
-gpu = cuda.get_current_device()
-print(f"maxThreadsPerBlock: {(MAX_THREADS_PER_BLOCK := gpu.MAX_THREADS_PER_BLOCK)}")
-print(f"maxBlockDimX:  {(MAX_BLOCK_DIM_X := gpu.MAX_BLOCK_DIM_X)}")
-print(f"maxBlockDimY:  {(MAX_BLOCK_DIM_Y := gpu.MAX_BLOCK_DIM_Y)}")
-print(f"maxBlockDimZ:  {(MAX_BLOCK_DIM_Z := gpu.MAX_BLOCK_DIM_Z)}")
-print(f"maxGridDimX:  {(MAX_GRID_DIM_X := gpu.MAX_GRID_DIM_X)}")
-print(f"maxGridDimY: {(MAX_GRID_DIM_Y := gpu.MAX_GRID_DIM_Y)}")
-print(f"maxGridDimZ: {(MAX_GRID_DIM_Z := gpu.MAX_GRID_DIM_Z)}")
-print(f"maxSharedMemoryPerBlock: {(MAX_SHARED_MEMORY_PER_BLOCK := gpu.MAX_SHARED_MEMORY_PER_BLOCK)}")
-print(f"asyncEngineCount: {(ASYNC_ENGINE_COUNT := gpu.ASYNC_ENGINE_COUNT)}")
-print(f"canMapHostMemory: {(CAN_MAP_HOST_MEMORY := gpu.CAN_MAP_HOST_MEMORY)}")
-print(f"multiProcessorCount: {(MULTIPROCESSOR_COUNT := gpu.MULTIPROCESSOR_COUNT)}")
-print(f"warpSize: {(WARP_SIZE := gpu.WARP_SIZE)}")
-FREE_MEMORY, TOTAL_MEMORY = cuda.current_context().get_memory_info()
-print(f"Free device memory: {FREE_MEMORY}")
-print(f"Total device memory: {TOTAL_MEMORY}")
-print(f"Occupied memory at cuda startup: {100*FREE_MEMORY/TOTAL_MEMORY:.2f}%")
 
-INF = 2**32-1
-VOL_EDGE = 500
-RADIUS =  VOL_EDGE/2 - 0.5
-VOXELS_PER_THREAD = math.ceil(VOL_EDGE/MAX_THREADS_PER_BLOCK)
-THREADS_PER_BLOCK = math.ceil(VOL_EDGE/VOXELS_PER_THREAD)
-A = np.zeros((VOL_EDGE, VOL_EDGE, VOL_EDGE), dtype = np.uint32)
-A[VOL_EDGE//4:3*VOL_EDGE//4, VOL_EDGE//4:3*VOL_EDGE//4, VOL_EDGE//4:3*VOL_EDGE//4] = 1
-print(VOXELS_PER_THREAD, THREADS_PER_BLOCK)
 BASE_DEVICE_MEMORY_USE = 5000000
+BYTES_PER_PIXEL = 4
+MEMORY_TOLERANCE_MARGIN = 1.1
 
 
 def edt_gpu(A):
@@ -59,11 +32,10 @@ def edt_gpu(A):
     return B
 
 
-def edt_gpu_split(A, min_splits=None):
+def edt_gpu_split(A, segments):
     gpu = cuda.get_current_device()
     max_threads_per_block = gpu.MAX_THREADS_PER_BLOCK
     x_dim, y_dim, z_dim = A.shape
-    segments = 2
     
     for grid_axis_1, grid_axis_2, line_axis, gedt_compiler in (
     (1, 2, 0, compile_gedt_x),
@@ -96,8 +68,6 @@ def edt_gpu_split(A, min_splits=None):
             A[ordered_slices[0], ordered_slices[1], ordered_slices[2]] = A_d.copy_to_host()
 
     del A_d
-    print(A.sum())
-    A.tofile("test.raw")
     return A
     
     
@@ -108,4 +78,41 @@ def edt_cpu(A):
     single_pass_erosion_z(B)
     return B
     
+
+def edt(A, force_method=None, minimum_segments=None):
+    if force_method == None:
+        method = _auto_decide_method(A)
+    elif force_method in ('cpu', 'gpu', 'gpu-split'):
+        method = force_method
+    else:
+        raise ValueError(f"force_method must be one of 'cpu', 'gpu' or 'gpu-split', was {force_method}")
+        
+    if method == "cpu":
+        function = edt_cpu
+    elif method == "gpu":
+        function = edt_gpu
+    elif method == "gpu-split":
+        free_memory, total_memory = cuda.current_context().get_memory_info()
+        expected_memory_use = (A.size*BYTES_PER_PIXEL + BASE_DEVICE_MEMORY_USE) * MEMORY_TOLERANCE_MARGIN
+        segments = math.ceil(math.sqrt(expected_memory_use/free_memory))
+        if minimum_segments:
+            segments = max(segments, minimum_segments)
+        function = lambda x: edt_gpu_split(x, segments)
+
+    return function(A)
+
+def run_benchmark():
+    pass
+
+
+def _auto_decide_method(A):
+    if not cuda.is_available():
+        return "cpu"
+        
+    free_memory, total_memory = cuda.current_context().get_memory_info()
+    expected_memory_use = (A.size*BYTES_PER_PIXEL + BASE_DEVICE_MEMORY_USE) * MEMORY_TOLERANCE_MARGIN
     
+    if free_memory <= expected_memory_use:
+        return "gpu"
+        
+    return "gpu-split"
--- tests/test_pyedt.py
+++ tests/test_pyedt.py
@@ -2,7 +2,7 @@ import pathlib
 
 import numpy as np
 
-from PyEDT.interface import edt_gpu, edt_gpu_split, edt_cpu
+from PyEDT.interface import edt_gpu, edt_gpu_split, edt_cpu, edt
 
 reference = np.fromfile(pathlib.Path(__file__).parent/"20_edt.raw", dtype=np.uint32).reshape(20, 20, 20)
 EDGE_SIZE = 20
@@ -19,7 +19,7 @@ def test_edt_gpu_split():
     array[EDGE_SIZE//4:3*EDGE_SIZE//4,
           EDGE_SIZE//4:3*EDGE_SIZE//4,
           EDGE_SIZE//4:3*EDGE_SIZE//4] = 1
-    assert(np.all(edt_gpu_split(array) == reference))
+    assert(np.all(edt_gpu_split(array, 2) == reference))
     
 def test_edt_cpu():
     array = np.zeros((EDGE_SIZE, EDGE_SIZE, EDGE_SIZE), dtype=np.uint32)
@@ -27,3 +27,10 @@ def test_edt_cpu():
           EDGE_SIZE//4:3*EDGE_SIZE//4,
           EDGE_SIZE//4:3*EDGE_SIZE//4] = 1
     assert(np.all(edt_cpu(array) == reference))
+    
+def test_edt():
+    array = np.zeros((EDGE_SIZE, EDGE_SIZE, EDGE_SIZE), dtype=np.uint32)
+    array[EDGE_SIZE//4:3*EDGE_SIZE//4,
+          EDGE_SIZE//4:3*EDGE_SIZE//4,
+          EDGE_SIZE//4:3*EDGE_SIZE//4] = 1
+    assert(np.all(edt(array) == reference))
